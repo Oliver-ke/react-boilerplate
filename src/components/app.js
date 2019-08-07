7132b5684ed4ccc222fc4783816eacdb7b509ba6
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-08-07 15:55:35 +0100

    Initial setup

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+	return (
+		<div className="full-screen">
+			<div>
+				<h1>React Page</h1>
+				<br />
+				<a className="button-line" href="https://github.com/oliver-ke/react-boilerplate" target="_blank">
+					Vesit repository
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default App;
